7d345781a19c492f92eb37bd08e5a6109ffd3f5c
Vikral Yadav <yadavvikral2003@gmail.com>
2025-11-06 19:41:07 +0530

    changes

--- agent.js
+++ agent.js
@@ -482,11 +482,27 @@ const calendarAgent = createAgent({
 
 
 
-const res =await calendarAgent.invoke({
-  messages: [{ role: "user", content: "Schedule a team meeting next Tuesday at 2pm for 1 hour" }]
+
+
+
+
+
+const query = "Schedule a team meeting next Tuesday at 2pm for 1 hour";
+
+const stream = await calendarAgent.stream({
+  messages: [{ role: "user", content: query }]
 });
 
+for await (const step of stream) {
+  for (const update of Object.values(step)) {
+    if (update && typeof update === "object" && "messages" in update) {
+      for (const message of update.messages) {
+        console.log(message.toFormattedString());
+      }
+    }
+  }
+}
 
 
-console.log(res);
+
 
